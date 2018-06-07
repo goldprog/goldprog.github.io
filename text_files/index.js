5939bfc00f4b8242800a7a24b8efaf69c89e776f
郭鹏1 <guopeng1@longtu.com>
2018-06-07 15:33:14 +0800

    sdf

--- text_files/index.js
+++ text_files/index.js
@@ -74,35 +74,31 @@ $(function(){
       loadon();
     }
 
-   //  if (navigator.userAgent.indexOf("MiuiBrowser")) {
-   //    loadVideo();
-   //    video.addClass('miui');
-   //  }else {
-   //    var req= new XMLHttpRequest();
-   //    req.open("GET", videoSrc, true);
-   //    req.responseType = "blob";
-   //    req.onload = function() {
-   //      if (200 === this.status && "video/mp4" === this.response.type) {
-   //        var res = this.response,
-   //          url = (window.URL || window.webkitURL || window || {}).createObjectURL(res);
-   //          console.log(res);
-   //        hideEle(loading);
-   //        showEle(playBtn);
-   //        video[0].src = url;
-   //      } else {
-   //        loadVideo();
-   //      }
-   //    }
-   //    req.onerror = function(e) {
-   //      console.log(e);
-   //      loadVideo();
-   //    };
-   //    req.send()
-   // }
-   video[0].addEventListener("canplay", function() {
-     hideEle(loading);
-     showEle(playBtn);
-　　}, false);
+    if (navigator.userAgent.indexOf("MiuiBrowser")!==-1) {
+      loadVideo();
+      video.addClass('miui');
+    }else {
+      var req= new XMLHttpRequest();
+      req.open("GET", videoSrc, true);
+      req.responseType = "blob";
+      req.onload = function() {
+        if (200 === this.status && "video/mp4" === this.response.type) {
+          var res = this.response,
+            url = (window.URL || window.webkitURL || window || {}).createObjectURL(res);
+            console.log(res);
+          hideEle(loading);
+          showEle(playBtn);
+          video[0].src = url;
+        } else {
+          loadVideo();
+        }
+      }
+      req.onerror = function(e) {
+        console.log(e);
+        loadVideo();
+      };
+      req.send()
+   }
     playBtn.click(function(){
       hideEle($('.all'));
       //showEle(video);
