8d8b062639d29db57ee063b906ccb10e57087f49
郭鹏1 <guopeng1@longtu.com>
2018-06-08 11:04:06 +0800

    sd

--- text_files/index.js
+++ text_files/index.js
@@ -60,7 +60,7 @@ $(function() {
   })();
   console.log(isPhone6p);
   if(isPhone6p){
-    video.css({"transform":"translate(-50%, -50%) scaleX(1.135)"})
+    video.css({"transform":"translate(-50%, -50%) scaleX(1.125)"})
   }
   //
   // progressHandler = function(e) {
