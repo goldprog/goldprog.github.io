9a471667ae281804f7db30b33ad38ca9f43519a8
郭鹏1 <guopeng1@longtu.com>
2018-06-12 14:01:59 +0800

    dsf

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,9 @@ $(function(){
   var video = $("#video");
   var loading = $("#loading"),
     content = $("#content"),
+    playBtnWrap = $('.play_wrap'),
     playBtn = $("#play"),
+    replayBtn = $('#refreshBtn'),
     videoSrc = "static/images/v0.mp4";
     //显示方式
     function showEle(el){
@@ -35,10 +37,23 @@ $(function(){
     }
     addSourceToVideo( video[0], videoSrc,"video/mp4");
 
+   var isPhone6p = (function(){
+   　var h=window.innerHeight,w=window.innerWidth,useragent = navigator.userAgent.toLowerCase(),isP6p = false;
+
+   　if(useragent.match(/mobile/i)!==null && useragent.match(/iphone/i)!==null && ( h>w ? (Math.abs(w-414)<10 && h<=736) : (Math.abs(w-736)<10) && h<=414)) isP6p = true;
+
+   　return isP6p;
+   })();
+
+   if(isPhone6p){
+     video.css({"transform": "translate(-50%, -50%) scaleX(1.08)"});
+
+   }
+
     var timer2 = setTimeout(function() {
       clearInterval(timer2);
       hideEle(loading);
-      showEle(playBtn);
+      showEle(playBtnWrap);
     }, 9000)
 
     playBtn.click(function(){
@@ -46,7 +61,13 @@ $(function(){
       showEle(video);
       video[0].currentTime = 0;
       video[0].play();
-    })
+    });
+    replayBtn.click(function(){
+      hideEle(content);
+      showEle(video);
+      video[0].play();
+    });
+
     video[0].addEventListener("timeupdate", function() {
       if(!video[0].isPlayed && this.currentTime > .1){
         showEle(video);
