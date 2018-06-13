1444210f776b1d9f73b845fb07bc9b45085a69a3
郭鹏1 <guopeng1@longtu.com>
2018-06-13 09:42:12 +0800

    sd

--- static/js/index.js
+++ static/js/index.js
@@ -29,14 +29,6 @@ $(function(){
       el.hide()
     }
 
-    function addSourceToVideo(element, src, type) {
-      var source = document.createElement('source');
-      source.src = src;
-      source.type = type;
-      element.appendChild(source);
-    }
-    addSourceToVideo( video[0], videoSrc,"video/mp4");
-
    var isPhone6p = (function(){
    　var h=window.innerHeight,w=window.innerWidth,useragent = navigator.userAgent.toLowerCase(),isP6p = false;
 
@@ -46,15 +38,79 @@ $(function(){
    })();
 
    if(isPhone6p){
-     video.css({"transform": "translate(-50%, -50%) scaleX(1.08)"});
-
+     video.css({"transform": "translate(-50%, -50%) scaleX(1.1)"});
    }
+    //加载视频
+    function loadVideo(){
 
-    var timer2 = setTimeout(function() {
-      clearInterval(timer2);
-      hideEle(loading);
-      showEle(playBtnWrap);
-    }, 9000)
+      function videoPause(){
+        video[0].pause();
+        video[0].removeEventListener("playing", loadVideo)
+      }
+
+      function loadComplete(){
+        hideEle(loading);
+        showEle(playBtnWrap);
+        if(timer){
+          clearTimeout(timer);
+          timer = null;
+        }
+      }
+
+      function videoBuffer(el){
+        var time = el.buffered.length > 0 ? el.buffered.end(0) : 0;
+        return time = parseInt(1000 * time + 1) / 1000;
+      }
+
+      function loadon(){
+        var alreadyTime = videoBuffer(video[0]),
+            duration = video[0].duration;
+        if(+ new Date - oldTime > delay || alreadyTime >= duration || y === alreadyTime){
+          loadComplete()
+        }else{
+          y = alreadyTime;
+          timer = setTimeout(function() {
+            loadon()
+          }, 500)
+        }
+      }
+
+      video[0].src = videoSrc;
+      var oldTime = +new Date,
+        delay = 4000,
+        timer = null;
+      video[0].play();
+      video[0].addEventListener("playing", loadVideo);
+      var y = -1;
+
+      loadon();
+    }
+
+    if (navigator.userAgent.indexOf("MiuiBrowser")!==-1) {
+      //loadVideo();
+      video.addClass('miui');
+    }else {
+      var req= new XMLHttpRequest();
+      req.open("GET", videoSrc, true);
+      req.responseType = "blob";
+      req.onload = function() {
+        if (200 === this.status && "video/mp4" === this.response.type) {
+          var res = this.response,
+          url = (window.URL || window.webkitURL || window || {}).createObjectURL(res);
+          console.log(res);
+          hideEle(loading);
+          showEle(playBtnWrap);
+          video[0].src = url;
+        } else {
+          loadVideo();
+        }
+      }
+      req.onerror = function(e) {
+        console.log(e);
+        loadVideo();
+      };
+      req.send()
+   }
 
     playBtn.click(function(){
       hideEle($('.all'));
